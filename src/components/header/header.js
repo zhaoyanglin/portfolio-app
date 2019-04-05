27cbe2206caeb91fd40b3366568744ad3f3dee08
zhaoyanglin <zhaoyanglin@msn.com>
2019-04-05 13:22:58 -0500

    updated Quotes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,7 +13,7 @@ export default class Header extends Component {
                             <li><a className="smoothscroll" href="#about">About</a></li>
                             <li><a className="smoothscroll" href="#resume">Resume</a></li>
                             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-                            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+                            <li><a className="smoothscroll" href="#testimonials">Favorite Quotes</a></li>
                             <li><a className="smoothscroll" href="#contact">Contact</a></li>
                         </ul> {/* end #nav */}
                     </nav> {/* end #nav-wrap */}
--- src/components/testimonials/testimonials.js
+++ src/components/testimonials/testimonials.js
@@ -8,27 +8,23 @@ export default class Testimonials extends Component {
                     <div className="text-container">
                         <div className="row">
                             <div className="two columns header-col">
-                                <h1><span>Client Testimonials</span></h1>
+                                <h1><span>Favorite Quotes</span></h1>
                             </div>
                             <div className="ten columns flex-container">
                                 <div className="flexslider">
                                     <ul className="slides">
                                         <li>
                                             <blockquote>
-                                                <p>Your work is going to fill a large part of your life, and the only way to be truly satisfied is
-                                                  to do what you believe is great work. And the only way to do great work is to love what you do.
-                                                  If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it.
-                      </p>
-                                                <cite>Steve Jobs</cite>
+                                                <p>People should pursue what they are passionate about. That will make them happier then pretty much anything else.
+                                                </p>
+                                                <cite>Elon Musk</cite>
                                             </blockquote>
                                         </li> {/* slide ends */}
                                         <li>
                                             <blockquote>
-                                                <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                                                  Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem
-                                                  nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris.
-                      </p>
-                                                <cite>Mr. Adobe</cite>
+                                                <p>People work better when they know what the goal is and why. It is important that people look forward to coming to work in the morning and enjoy the work.
+                                                </p>
+                                                <cite>Elon Musk</cite>
                                             </blockquote>
                                         </li> {/* slide ends */}
                                     </ul>
