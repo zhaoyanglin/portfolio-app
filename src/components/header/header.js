2e7eb5641aa308f7e79bc18d38445733081e437d
zhaoyanglin <zhaoyanglin@msn.com>
2019-04-07 16:27:45 -0500

    most updated

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -20,9 +20,9 @@ export default class Header extends Component {
                     <div className="row banner">
                         <div className="banner-text">
                             <h1 className="responsive-headline">Zhaoyang Lin.</h1>
-                            <h3>I'm a Minnesota Twin cities based <span>Software Engineer</span>, <span>Programer</span> and <span>Software Developer</span> love to create amazing and
-              effective Web-app/ phone-app for companies of all sizes around the globe. Let's <a className="smoothscroll" href="#about">start scrolling</a>
-                                , and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
+                            <h3>I'm a Minnesota Twin cities based <span>Full-Stack Software Engineer</span>, who loves creating robust and
+                            seamless web and mobile applications.
+                            </h3>
                             <hr />
                             <ul className="social">
 
--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -29,7 +29,7 @@ export default class Portfolio extends Component {
                                 <div className="columns portfolio-item">
                                     <div className="item-wrap">
                                         <a href="#modal-02" title>
-                                            <img alt src="images/portfolio/newModals/garden.png" />
+                                            <img alt src="images/portfolio/newModals/recent.png" />
                                             <div className="overlay">
                                                 <div className="portfolio-item-meta">
                                                     <h5>Redux-Saga-Garden</h5>
@@ -55,21 +55,7 @@ export default class Portfolio extends Component {
                                         </a>
                                     </div>
                                 </div> {/* item end */}
-                                {/* item4 */}
-                                <div className="columns portfolio-item">
-                                    <div className="item-wrap">
-                                        <a href="#modal-04" title>
-                                            <img alt src="images/portfolio/into-the-light.jpg" />
-                                            <div className="overlay">
-                                                <div className="portfolio-item-meta">
-                                                    <h5>Into The Light</h5>
-                                                    <p>Photography</p>
-                                                </div>
-                                            </div>
-                                            <div className="link-icon"><i className="icon-plus" /></div>
-                                        </a>
-                                    </div>
-                                </div> {/* item end */}
+
                             </div> {/* portfolio-wrapper end */}
                         </div> {/* twelve columns end */}
                         {/* Modal Popup
@@ -113,18 +99,6 @@ export default class Portfolio extends Component {
                             </div>
                         </div>{/* modal-03 End */}
 
-                        <div id="modal-04" className="popup-modal mfp-hide">
-                            <img className="scale-with-grid" src="images/portfolio/modals/m-intothelight.jpg" alt />
-                            <div className="description-box">
-                                <h4>Into the Light</h4>
-                                <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
-                                <span className="categories"><i className="fa fa-tag" />Photography</span>
-                            </div>
-                            <div className="link-box">
-                                <a href="http://www.behance.net">Details</a>
-                                <a className="popup-modal-dismiss">Close</a>
-                            </div>
-                        </div>{/* modal-04 End */}
                     </div> {/* row End */}
                 </section>
             </React.Fragment>
