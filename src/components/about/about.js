cf9040e6b5934452b9e236768fe7397f2da91853
zhaoyanglin <zhaoyanglin@msn.com>
2019-07-02 21:29:26 -0500

    update

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -27,7 +27,7 @@ export default class About extends Component {
                                 </div>
                                 <div className="columns download">
                                     <p>
-                                        <a href="https://www.slideshare.net/slideshow/embed_code/key/w3zPJxPswUpeCn" className="button"><i className="fa fa-download" />Download Resume</a>
+                                        <a href="https://www.slideshare.net/slideshow/embed_code/key/IDBjyVLpYqBXpi" className="button"><i className="fa fa-download" />Download Resume</a>
                                     </p>
                                 </div>
                             </div>
